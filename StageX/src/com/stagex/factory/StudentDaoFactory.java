89b23b1416bf2fe99bdb2f72dce0cf3081e35852
SD-201608282042\Administrator <wadeleee128@gmail.com>
2016-12-08 16:18:31 +0100

    li jiajie finish the most commom find delete create update of all tables coding

---
+++ StageX/src/com/stagex/factory/StudentDaoFactory.java
@@ -0,0 +1,8 @@
+package com.stagex.factory;
+
+import com.stagex.bean.Student;
+import com.stagex.dao.*;
+
+public class StudentDaoFactory extends GenericDaoImpl<Student>{
+	//write student own sql method
+}
---
+++ StageX/src/com/stagex/factory/TeacherDaoFactory.java
@@ -0,0 +1,8 @@
+package com.stagex.factory;
+
+import com.stagex.bean.Teacher;
+import com.stagex.dao.GenericDaoImpl;
+
+public class TeacherDaoFactory extends GenericDaoImpl<Teacher> {
+
+}
